16f8e205de890ac443cc6df1b0b49726e941e523
JonatanC <jonatancotiz40@gmail.com>
2025-10-02 19:38:59 -0400

    new challenges completed

---
+++ hackerand-problems/1.py
@@ -0,0 +1,54 @@
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Explanation 0
+
+
+is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation 1
+
+
+and  is even, so it is not weird.
+"""
+
+
+if __name__ == '__main__':
+    n = [3,24]
+
+    for number in n:
+        if number % 2 == 0:
+            if number in range(2, 6) or number > 20:
+                print("Not Weird")
+            elif number in range(6, 21):
+                print("Weird")
+        else:
+            print("Weird")
